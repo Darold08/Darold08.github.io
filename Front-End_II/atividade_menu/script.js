15974e385887e934db7d7fb16362f60ed586980c
Darold08 <liviaadarold17@gmail.com>
2024-10-30 08:23:16 -0400

    dom

--- Front-End_II/atividade_menu/script.js
+++ Front-End_II/atividade_menu/script.js
@@ -1,40 +1,47 @@
-const body = document.querySelector('body');
-const head = document.createElement('head');
-
 const itensMenu = [
     { nome: 'Início', url: 'index.html' },
     { nome: 'Sobre', url: 'sobre.html' },
     { nome: 'Contato', url: 'contato.html' }
-];
+]
 
-function estruturaHTML(){
+function criarHTML() {
+    const body = document.querySelector('body')
     const header = document.createElement('header')
-    const nav = document.createElement('nav')
+    let nav = document.createElement('nav')
     const ul = document.createElement('ul')
     body.appendChild(header)
     header.appendChild(nav)
     nav.appendChild(ul)
-    ul.style.backgroundColor = 'black'
-
-    return(ul)
 }
 
-
-function adicionaritem(ul){
-
+function addItem() {
     itensMenu.forEach(item => {
-        let ancora = document.createElement('a')
-        let li = document.createElement('li')
-        ancora.setAttribute('href', item.url)
-        ancora.textContent = item.nome.toLocaleUpperCase()
-        ancora.style.color = 'white'
-        ancora.style.textDecoration = 'none'
-        ul.appendChild(li)
-        li.appendChild(ancora)
-        li.style.marginLeft
+        const ul = document.querySelector('ul')
+        let itens = document.createElement('li');
+        let ancora = document.createElement('a');
+        ancora.setAttribute('href', item.url);
+        ancora.setAttribute('target', '_blank');
+        ul.appendChild(itens);
+        itens.appendChild(ancora);
+        ancora.textContent = item.nome.toUpperCase();
+        ancora.style.color = '#fff';
+        ancora.style.textDecoration = 'none';
     })
-
-
 }
 
-adicionaritem(estruturaHTML())
+function estilizarMenu() {
+    const header = document.querySelector('header');
+    const ul = document.querySelector('ul');
+    header.style.backgroundColor = '#071D41'; 
+    header.style.fontFamily = 'Arial'; 
+    ul.style.display = 'flex';
+    ul.style.flexDirection = 'row';
+    ul.style.justifyContent = 'center'; 
+    ul.style.alignItems = 'center'; 
+    ul.style.height = '40px'; 
+    ul.style.gap = '20px'; 
+    ul.style.listStyleType = 'none';
+}
+
+addItem(criarHTML());
+estilizarMenu()

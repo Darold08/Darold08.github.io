f34cfe9b11643b85968da761ae88868ab94eca48
Darold08 <liviaadarold17@gmail.com>
2024-11-08 08:23:54 -0400

    ..

--- Front-End_II/projeto/src/app/layout.js
+++ Front-End_II/projeto/src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Header from "@/components/Header";
+ 
 export const metadata = {
   title: "Minha primeira aplicação",
   description: "Aplicação de Front-end II",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
+        <Header />
         {children}
       </body>
     </html>
